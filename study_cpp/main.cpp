5a56ac1a9e58643de5e1506b9db56a5f54f785ab
Eunhwa Yi <eunhwa10216@naver.com>
2026-02-24 13:01:41 +0900

    Study vector, forward_list, iterator

--- study_cpp/main.cpp
+++ study_cpp/main.cpp
@@ -1,11 +1,87 @@
 #include "main.h"
 
+#include <string>
 #include <iostream>
-#include <array>
-#include <type_traits>
+#include <forward_list>
+#include <vector>
 
+//연습문제 4:; 다양한 반복자에서 이동하기
+int main()
+{
+	// vector iterator
+	std::vector<std::string> vec = {
+		"Lewis Hamilton", "Lewis Hamilton", "Nico Roseberg",
+		"Sebastian Vettel", "Lewis Hamilton", "Sebastian Vettel",
+		"Sebastian Vettel", "Sebastian Vettel", "Fernando Alonso"
+	};
+
+	auto vec_it = vec.begin(); // 상수 시간(입력한 것과 상관없이 일정한 시간복잡도를 가짐)(O(1))
+	std::cout << "The most resent winner : " << *vec_it << std::endl;
+
+	vec_it = vec_it + 8;
+	std::cout << "The winner eight years ago : " << *vec_it << std::endl;
+
+	advance(vec_it, -3); // advance(반복자, 거리 값) : 반복자에서 지정한 거리만큼 떨어진 위치의 반복자를 반환
+	std::cout << "The winner three years after : " << *vec_it << std::endl;
+
+	// forward_list iterator
+	std::forward_list<std::string> fwd(vec.begin(), vec.end());
+
+	auto list_it = fwd.begin(); // 선형 시간
+	std::cout << "The winner eight years ago : " << *list_it << std::endl;
+
+	advance(list_it, 5); 
+	std::cout << "The winner five years ago : " << *list_it << std::endl;
+
+	//forward_list는 순방향으로만 이동 가능, advance(list_it, -2)는 에러 발생
+}
+
+/*
+//연습문제 3: 연결 리스트에서 remove_if() 함수를 이용한 조건부 원소 삭제 
+struct citizen
+{
+	std::string name;
+	int age;
+};
+
+std::ostream &operator<<(std::ostream& os, const citizen& c)
+{
+	return (os << "[" << c.name << ", " << c.age << "]");
+}
+
+int main()
+{
+	std::forward_list<citizen> citizens =
+	{
+		{"Kim", 22}, {"Lee", 25}, {"Park", 18}, {"Jin", 16}
+	};
+
+	auto citizens_copy = citizens;
+
+	std::cout << "All citizens : ";
+	for (const auto& c : citizens)
+		std::cout << c << " ";
+	std::cout << std::endl;
+
+	citizens.remove_if([](const citizen &c) { return (c.age < 19); });
+
+	std::cout << "Votable citizen: ";
+	for (const auto& c : citizens)
+		std::cout << c << " ";
+	std::cout << std::endl;
+
+	citizens_copy.remove_if([](const citizen& c) {return (c.age != 18); });
+	
+	std::cout << "Votable next year citizen: ";
+	for (const auto& c : citizens_copy)
+		std::cout << c << " ";
+	std::cout << std::endl;
+}
+*/
+
+/*
 //연습문제 2: 빠르고 범용적인 데이터 저장 컨테이너 만들기 
-template<typename ... Args>
+template<typename ... Args> // 가변 길이 템플릿
 auto build_array(Args&&... args) -> std::array<typename std::common_type<Args...>::type, sizeof...(args)>
 // 후행 리턴 타입(trailing return type) : 함수의 반환 타입을 함수 매개변수 목록 뒤에 명시하는 방법
 {
@@ -22,7 +98,7 @@ int main()
 	std::cout << std::endl;
 }
 
-/*
+
 //연습문제1 : 동적 크기 배열 구현
 template <typename T>
 class dynamic_array
