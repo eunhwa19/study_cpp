b9c2f72cbe4c6010cc0a95630017dd96727954a3
Eunhwa Yi <eunhwa10216@naver.com>
2026-03-12 13:32:36 +0900

    2026.03.12 Study linked list

--- study_cpp/main.cpp
+++ study_cpp/main.cpp
@@ -1,10 +1,143 @@
 #include "main.h"
 
-#include <string>
 #include <iostream>
-#include <forward_list>
-#include <vector>
+#include <algorithm>
 
+//연습문제 5 : 기본적인 사용자 정의 컨테이너 만들기
+struct singly_ll_node //singly linked list 
+{
+	int data;
+	singly_ll_node* next;
+};
+
+class singly_ll
+{
+public:
+	using node = singly_ll_node;
+	using node_ptr = node*;
+
+private:
+	node_ptr head;
+
+public: 
+	void push_front(int val)
+	{
+		auto new_node = new node{ val, NULL };
+		if (head != NULL)
+			new_node->next = head;
+		head = new_node;
+	}
+
+	void pop_front()
+	{
+		auto first = head;
+		if (head)
+		{
+			head = head->next;
+			delete first;
+		}
+	}
+
+	struct singly_ll_iterator
+	{
+	private:
+		node_ptr ptr;
+
+	public:
+		singly_ll_iterator(node_ptr p) : ptr(p) {} //constructor
+
+		int& operator*() { return ptr->data; } //accessor
+
+		node_ptr get() { return ptr; }
+
+		singly_ll_iterator& operator++()
+		{
+			ptr = ptr->next;
+			return *this;
+		}
+
+		singly_ll_iterator operator++(int)
+		{
+			singly_ll_iterator result = *this;
+			++(*this);
+			return result;
+		}
+
+		friend bool operator==(const singly_ll_iterator& left, const singly_ll_iterator& right)
+		{
+			return left.ptr == right.ptr;
+		}
+
+		friend bool operator!=(const singly_ll_iterator& left, const singly_ll_iterator& right)
+		{
+			return left.ptr != right.ptr;
+		}
+	};
+
+	singly_ll_iterator begin() { return singly_ll_iterator(head); }
+	singly_ll_iterator end() { return singly_ll_iterator(NULL); }
+	singly_ll_iterator begin() const { return singly_ll_iterator(head); }
+	singly_ll_iterator end() const { return singly_ll_iterator(NULL); }
+
+	singly_ll() = default;
+
+	singly_ll(const singly_ll& other) : head(NULL)
+	{
+		if (other.head)
+		{
+			head = new node{ 0, NULL };
+			auto cur = head;
+			auto it = other.begin();
+			while (true)
+			{
+				cur->data = *it;
+
+				auto tmp = it;
+				++tmp;
+				if (tmp == other.end())
+					break;
+				
+				cur->next = new node{ 0, NULL };
+				cur = cur->next;
+				it = tmp;
+			}
+		}
+	}
+
+	singly_ll(const std::initializer_list<int>& ilist) : head(NULL)
+	{
+		for (auto it = std::rbegin(ilist); it != std::rend(ilist); it++)
+			push_front(*it);
+	}
+};
+
+int main()
+{
+	singly_ll sll = { 1, 2, 3 };
+	sll.push_front(0);
+
+	std::cout << "첫 번째 리스트 : ";
+	for (auto i : sll)
+		std::cout << i << " ";
+	std::cout << std::endl;
+
+	auto sll2 = sll;
+	sll2.push_front(-1);
+	std::cout << "첫 번째 리스트를 복사한 후, 맨 앞에 -1을 추가: ";
+	for (auto i : sll2)
+		std::cout << i << ' ';
+	std::cout << std::endl;
+
+	std::cout << "깊은 복사 후 첫 번째 리스트: ";
+
+	for (auto i : sll)
+		std::cout << i << ' ';
+	std::cout << std::endl;
+
+	return 0;
+}
+
+/*
 //연습문제 4:; 다양한 반복자에서 이동하기
 int main()
 {
@@ -35,6 +168,7 @@ int main()
 
 	//forward_list는 순방향으로만 이동 가능, advance(list_it, -2)는 에러 발생
 }
+*/
 
 /*
 //연습문제 3: 연결 리스트에서 remove_if() 함수를 이용한 조건부 원소 삭제 
